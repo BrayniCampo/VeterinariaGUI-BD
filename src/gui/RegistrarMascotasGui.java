8dfa5e806a2706ca1f17576ca27044c870ced364
Brayni Campo <braynivalentina@gmail.com>
2022-03-16 21:22:11 -0500

    first commit

---
+++ src/gui/RegistrarMascotasGui.java
@@ -0,0 +1,160 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.EmptyBorder;
+
+import controlador.Coordinador;
+import vo.MascotaVo;
+import vo.PersonaVo;
+
+public class RegistrarMascotasGui extends JDialog implements ActionListener{
+
+	private final JPanel contentPanel=new JPanel();
+	private JTextField txtIdDueno;
+	private JTextField txtNombre;
+	private JTextField txtRaza;
+	private JButton btnCancelar;
+	private JButton btnRegistrar;
+	private Coordinador miCoordinador;
+	private JComboBox comboxColor;
+	private JLabel lblColor;
+	private JComboBox comboxSexo;
+	private JLabel lblSexo;
+	private JLabel lblRaza;
+	private JLabel lblNombre;
+	private JLabel lblDueno;
+	private JLabel lblIdMascota;
+	private JTextPane txtIdMascota;
+	
+	
+	public RegistrarMascotasGui(VentanaPrincipal ventanaPrincipal, boolean modal, String documento) {
+		super(ventanaPrincipal,modal);
+		setSize( 408, 288);
+		setLocationRelativeTo(null);
+		getContentPane().setLayout(new BorderLayout());
+		setTitle("Gestion de Mascotas");
+		
+		iniciarComponentes(documento);
+		
+		
+	}
+
+
+	private void iniciarComponentes(String documento) {
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("GESTIONAR MASCOTAS");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Tw Cen MT", Font.BOLD, 20));
+		lblTitulo.setBounds(10, 10, 372, 28);
+		contentPanel.add(lblTitulo);
+				
+		JPanel panel = new JPanel();
+		panel.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
+		panel.setBounds(10, 49, 370, 192);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		lblDueno = new JLabel("Id Due\u00F1o:");
+		lblDueno.setBounds(199, 17, 71, 21);
+		panel.add(lblDueno);
+		
+		txtIdDueno = new JTextField();
+		txtIdDueno.setText(documento);
+		txtIdDueno.setBounds(269, 17, 86, 20);
+		panel.add(txtIdDueno);
+		txtIdDueno.setColumns(10);
+		
+		lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(24, 49, 71, 21);
+		panel.add(lblNombre);
+		
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(88, 49, 86, 20);
+		panel.add(txtNombre);
+		
+		lblRaza = new JLabel("Raza:");
+		lblRaza.setBounds(214, 49, 71, 21);
+		panel.add(lblRaza);
+		
+		txtRaza = new JTextField();
+		txtRaza.setColumns(10);
+		txtRaza.setBounds(269, 49, 86, 20);
+		panel.add(txtRaza);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(24, 127, 331, 12);
+		panel.add(separator);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(this);
+		btnCancelar.setBounds(271, 150, 89, 23);
+		panel.add(btnCancelar);
+		
+		btnRegistrar = new JButton("Registrar");
+		btnRegistrar.setBounds(172, 150, 89, 23);
+		btnRegistrar.addActionListener(this);
+		panel.add(btnRegistrar);
+		
+		lblSexo = new JLabel("Sexo:");
+		lblSexo.setBounds(24, 81, 71, 21);
+		panel.add(lblSexo);
+		
+		comboxSexo = new JComboBox();
+		comboxSexo.setModel(new DefaultComboBoxModel(new String[] {"Macho", "Hembra"}));
+		comboxSexo.setBounds(88, 81, 86, 22);
+		panel.add(comboxSexo);
+		
+		lblColor = new JLabel("Color:");
+		lblColor.setBounds(214, 82, 71, 21);
+		panel.add(lblColor);
+		
+		comboxColor = new JComboBox();
+		comboxColor.setModel(new DefaultComboBoxModel(new String[] {"Blanco", "Negro", "Caf\u00E9", "Gris", "Manchas"}));
+		comboxColor.setBounds(269, 81, 86, 22);
+		panel.add(comboxColor);
+		
+		lblIdMascota = new JLabel("Id Mascota ");
+		lblIdMascota.setBounds(24, 17, 61, 13);
+		panel.add(lblIdMascota);
+		
+		txtIdMascota = new JTextPane();
+		txtIdMascota.setBounds(95, 17, 79, 21);
+		panel.add(txtIdMascota);
+	
+	}
+
+	public void setCoordinador(Coordinador miCoordinador) {
+		this.miCoordinador=miCoordinador;
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		if(e.getSource()==btnRegistrar) {
+			
+			MascotaVo miAnimal = new MascotaVo();
+			
+			miAnimal.setIdMascota(Long.parseLong(txtIdMascota.getText()));
+			miAnimal.setNombre(txtNombre.getText());
+			miAnimal.setRaza(txtRaza.getText());
+			miAnimal.setSexo((String) comboxSexo.getSelectedItem());
+			miAnimal.setColorMascota((String) comboxColor.getSelectedItem());
+			miAnimal.setIdDueno(txtIdDueno.getText());
+			
+			
+			PersonaVo miPersonaVo = miCoordinador.setConsultarPersona(miAnimal.getIdMascota());
+		}
+	}
+}
